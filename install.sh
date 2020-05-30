b613debc2ce8ff5553c81dff52bf305449635b98
alexandremendoncaalvaro <alexandre.alvaro@hotmail.com>
2020-05-30 16:16:59 -0300

    git clone

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -u
 
+git clone https://github.com/alexandremendoncaalvaro/soft-power-button-raspberry.git ~/soft-power-button
+cd ~/soft-power-button
+
 sudo cp listen-for-shutdown.py /usr/local/bin/
 sudo cp listen-for-shutdown.sh /etc/init.d/
 
